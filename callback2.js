bfd46abd6a82ba4d81eccfe71eb541f43776b9be
unknown <kamatabhishek14@gmail.com>
2023-10-01 19:01:24 +0530

    added some more info abt sort function

--- callback2.js
+++ callback2.js
@@ -3,7 +3,8 @@ let arr = [100,5,4,200,14,0];
 
 arr.sort(function (a,b){
     return a-b
-})                 // sort also is an higher order function beacuse it actually takes COMPARATOR FUNCTION inside the arguments.
+})                 // sort also is an higher order function beacuse it actually takes COMPARATOR FUNCTION inside the arguments what it does is instead of
+                   // sorting in form of lexical order it sorts numerically .
 
 
 console.log(arr)
