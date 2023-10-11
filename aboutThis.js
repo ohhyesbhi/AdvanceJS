8c06435458ded54070d05a1a7a46c859391a01c8
unknown <kamatabhishek14@gmail.com>
2023-10-11 17:13:45 +0530

    added about this keyword in js

---
+++ aboutThis.js
@@ -0,0 +1,26 @@
+const iphone = {
+    name : "iphone",
+    price : "100000",
+    display(){
+        console.log(this)
+    }
+}
+
+const macbook = {
+    name : "macbook",
+    price : "120000",
+    display(){
+        console.log(this)
+    }
+}
+
+macbook.display()
+
+// o/p -: { name: 'macbook', price: '120000', display: [Function: display] }
+
+iphone.display()
+
+// o/p -: { name: 'iphone', price: '100000', display: [Function: display] }
+
+// here actually this refers to calling context as we can see , iphone is a 
+// calling context that i am calling display in the context of iphone.
