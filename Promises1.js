d57c2c96700d56cb37dc6ca4e4eac7c27cf0e6a4
unknown <kamatabhishek14@gmail.com>
2023-10-12 18:35:16 +0530

    added one corner case in promise

--- Promises1.js
+++ Promises1.js
@@ -17,4 +17,29 @@ function createPromise(){
 }
 
 let ans = createPromise()
-console.log(ans)
+// console.log(ans)
+
+function createPromise1(){
+    return new Promise(function (resolve,reject){
+        for(let i = 0 ; i < 10000000 ; i++){}
+        let x = getRandomInt(10);
+        if(x%2 == 0){
+            resolve(x)
+            resolve(10);
+            console.log("resolving again")
+        }else{
+            reject(x)
+            reject(10);
+            console.log("rejecting again")
+        }
+}
+    )
+}
+
+let ans1 = createPromise1()
+console.log(ans1)
+
+// o/p -: resolving again 
+//        Promise { 4 }
+
+//REASON-: So promise is resolved or Rejected only once 
